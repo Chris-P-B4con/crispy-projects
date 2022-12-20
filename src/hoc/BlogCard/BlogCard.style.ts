791832fd813cf3399f726a6fdcb656a94655d964
Christian Schlaiss <christian.schlaiss@maibornwolff.de>
2022-12-20 12:12:11 +0100

    #5: added hoc blog card which is preview of article

---
+++ src/hoc/BlogCard/BlogCard.style.ts
@@ -0,0 +1,22 @@
+import Button from '@mui/material/Button';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  cursor: pointer;
+  transition: 300ms all ease-in-out;
+  max-width: 350px;
+  background-color: ${(props) => props.theme.palette.background.level1};
+  box-shadow: var(--shadow-elevation-medium);
+  border-radius: var(--border-radius);
+  &:hover {
+    box-shadow: var(--shadow-elevation-high);
+  }
+  display: flex;
+`;
+
+export { Button, CardActions, CardContent, CardMedia, Typography };
---
+++ src/hoc/BlogCard/BlogCard.test.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import BlogCard from './BlogCard';
+
+it('BlogCard should mount', () => {
+  // const div = document.createElement('div');
+  // ReactDOM.render(<BlogCard />, div);
+  // ReactDOM.unmountComponentAtNode(div);
+});
