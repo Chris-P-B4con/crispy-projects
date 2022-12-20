820d25e4eafec0784b87f6715f30814b6d0b718d
Christian Schlaiss <christian.schlaiss@maibornwolff.de>
2022-12-20 17:20:09 +0100

    #10: added project page which lists all current projects

---
+++ src/pages/Projects/Projects.static.tsx
@@ -0,0 +1,72 @@
+import { ProjectInterface } from "./Projects"
+
+export const dummyProjectList: ProjectInterface[] = [
+    {
+      image: {
+        alt: 'coffee',
+        url: 'https://www.foodandwine.com/thmb/F_vjCR6V5D8lAknKCQB5nQFXV9Y=/750x0/filters:no_upscale():max_bytes(150000):strip_icc()/how-to-store-coffee-beans-FT-BLOG0121-99af87cb1f104993b234e1522746e82c.jpg',
+      },
+      title: 'Making Coffee',
+      subTitle: '10.12.2022 - Aachen',
+
+      // Author of article
+      author: {
+        alt: 'Christian Schlaiss',
+        src: 'Broken',
+      },
+      description:
+        'Making coffee is a delicate and lengthy process.. blawbefliwjefliwnemfowe fiowefj owef iowefmwoeifmw oefmwefoimw efoiwem foiw mfeowie fmowif iwoef',
+      tags: ['Coffee'],
+    },
+    {
+      image: {
+        alt: 'coffee',
+        url: 'https://www.foodandwine.com/thmb/F_vjCR6V5D8lAknKCQB5nQFXV9Y=/750x0/filters:no_upscale():max_bytes(150000):strip_icc()/how-to-store-coffee-beans-FT-BLOG0121-99af87cb1f104993b234e1522746e82c.jpg',
+      },
+      title: 'Making Coffee',
+      subTitle: '10.12.2022 - Aachen',
+
+      // Author of article
+      author: {
+        alt: 'Christian Schlaiss',
+        src: 'Broken',
+      },
+      description:
+        'Making coffee is a delicate and lengthy process.. blawbefliwjefliwnemfowe fiowefj owef iowefmwoeifmw oefmwefoimw efoiwem foiw mfeowie fmowif iwoef',
+      tags: ['Coffee'],
+    },
+    {
+      image: {
+        alt: 'coffee',
+        url: 'https://www.foodandwine.com/thmb/F_vjCR6V5D8lAknKCQB5nQFXV9Y=/750x0/filters:no_upscale():max_bytes(150000):strip_icc()/how-to-store-coffee-beans-FT-BLOG0121-99af87cb1f104993b234e1522746e82c.jpg',
+      },
+      title: 'Making Coffee',
+      subTitle: '10.12.2022 - Aachen',
+
+      // Author of article
+      author: {
+        alt: 'Christian Schlaiss',
+        src: 'Broken',
+      },
+      description:
+        'Making coffee is a delicate and lengthy process.. blawbefliwjefliwnemfowe fiowefj owef iowefmwoeifmw oefmwefoimw efoiwem foiw mfeowie fmowif iwoef',
+      tags: ['Coffee'],
+    },
+    {
+      image: {
+        alt: 'coffee',
+        url: 'https://www.foodandwine.com/thmb/F_vjCR6V5D8lAknKCQB5nQFXV9Y=/750x0/filters:no_upscale():max_bytes(150000):strip_icc()/how-to-store-coffee-beans-FT-BLOG0121-99af87cb1f104993b234e1522746e82c.jpg',
+      },
+      title: 'Making Coffee',
+      subTitle: '10.12.2022 - Aachen',
+
+      // Author of article
+      author: {
+        alt: 'Christian Schlaiss',
+        src: 'Broken',
+      },
+      description:
+        'Making coffee is a delicate and lengthy process.. blawbefliwjefliwnemfowe fiowefj owef iowefmwoeifmw oefmwefoimw efoiwem foiw mfeowie fmowif iwoef',
+      tags: ['Coffee'],
+    },
+  ]
