768dd98cb2f7b9e966d3c72f6ece9fbbc7cdebbf
Christian Schlaiss <christian.schlaiss@maibornwolff.de>
2022-12-21 07:44:17 +0100

    #20: implemented tags and cleaned up code

--- src/lib/globalVariables.js
+++ src/lib/globalVariables.js
@@ -3,44 +3,48 @@ export const COLORS = {
     primary: {
       main: '#ab47bc',
       light: '#df78ef',
-      dark: '#790e8b'
+      dark: '#790e8b',
     },
     secondary: {
       main: '#ce93d8',
       light: '#ffc4ff',
-      dark: '#9c64a6'
+      dark: '#9c64a6',
     },
     text: {
       primary: '#ffffff',
       secondary: 'rgba(255, 255, 255, 0.7)',
-      disabled: 'rgba(255, 255, 255, 0.5)'
+      disabled: 'rgba(255, 255, 255, 0.5)',
     },
     error: '#E2231A',
     buttonLabelActive: '#DCF8FF',
-    background: 'rgb(26, 32, 39)',
-    level1: 'rgb(18, 18, 18)',
-    level2: '#424242'
+    background: 'rgb(18, 18, 18)',
+    level1: 'rgb(26, 32, 39)',
+    level2: '#424242',
   },
   light: {
     primary: {
       main: '#ab47bc',
       light: '#df78ef',
-      dark: '#790e8b'
+      dark: '#790e8b',
     },
     secondary: {
       main: '#ce93d8',
       light: '#ffc4ff',
-      dark: '#9c64a6'
+      dark: '#9c64a6',
     },
     text: {
       primary: 'rgba(0, 0, 0, 0.87)',
       secondary: 'rgba(0, 0, 0, 0.6)',
-      disabled: 'rgba(0, 0, 0, 0.38)'
+      disabled: 'rgba(0, 0, 0, 0.38)',
     },
     error: '#E2231A',
     buttonLabelActive: '#DCF8FF',
     background: '#E7EBF0',
     level1: '#FFFFFF',
-    level2: '#424242'
-  }
+    level2: '#424242',
+  },
+};
+
+export const TAGS = {
+  coffee: '#6F4E37',
 };
